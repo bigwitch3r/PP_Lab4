fa75ed5055ee62466b234015d3cf483bf68b65fe
Danil Goncharov <bigwitch3r@gmail.com>
2022-05-30 21:46:58 +0300

    Added general task 2

--- general_task_2.cpp
+++ general_task_2.cpp
@@ -16,15 +16,46 @@ void general_task_two(int* argc, char** argv)
 
     int matrix[4][4];
 
-    for (int i = 0; i < 4; i++)
+    if (rank == 0)
     {
-        for (int j = 0; j < 4; j++)
+
+        printf("Generated matrix:\n");
+
+        for (int i = 0; i < 4; i++)
         {
-            matrix[i][j] = std::rand();
+            for (int j = 0; j < 4; j++)
+            {
+                matrix[i][j] = std::rand();
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
         }
     }
 
-    
+    int* buffer = (int*)malloc(sizeof(int) * 4);
+
+    MPI_Scatter(matrix, 4, MPI_INT, buffer, 4, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int raw_sum = buffer[0] + buffer[1] + buffer[2] + buffer[3];
+
+    int* raw_sums = NULL;
+
+    if (rank == 0)
+    {
+        raw_sums = (int*)malloc(sizeof(int) * numtasks);
+    }
+
+    MPI_Gather(&raw_sum, 1, MPI_INT, raw_sums, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        printf("Total sums:\n");
+
+        for (int i = 0; i < 4; i++)
+        {
+            printf("%d ", raw_sums[i]);
+        }
+    }
 
     MPI_Finalize();
 }
