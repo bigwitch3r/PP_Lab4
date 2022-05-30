3c0be8b80cfe2b86b02682ad06f6cc9908eb4a93
Гончаров Данил <bigwitch3r@gmail.com>
2022-05-30 18:30:55 +0300

    Update

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int* argc, char** argv)
+{
+    MPI_Init(argc, &argv);
+
+    // Получаем ранг в коммуникаторе
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    // Определяем корневой процесс
+    int broadcast_root = 0;
+
+    int buffer;
+
+    if (my_rank == broadcast_root)
+    {
+        buffer = 12345;
+        printf("[MPI process %d] I am the broadcast root, and send value %d.\n", my_rank, buffer);
+    }
+
+    MPI_Bcast(&buffer, 1, MPI_INT, broadcast_root, MPI_COMM_WORLD);
+
+    if (my_rank != broadcast_root)
+    {
+        printf("[MPI process %d] I am a broadcast receiver, and obtained value %d.\n", my_rank, buffer);
+    }
+
+    MPI_Finalize();
+}
